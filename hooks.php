b6810ad641b2236a7537caa4e5c022aec330d758
digger <digger@mysmf.net>
2020-05-20 13:40:00 +0400

    v 2.2

---
+++ hooks.php
@@ -0,0 +1,32 @@
+<?php
+
+if (file_exists(dirname(__FILE__) . '/SSI.php') && !defined('SMF'))
+	require_once(dirname(__FILE__) . '/SSI.php');
+elseif(!defined('SMF'))
+	die('<b>Error:</b> Cannot install - please verify that you put this file in the same place as SMF\'s index.php and SSI.php files.');
+
+if ((SMF == 'SSI') && !$user_info['is_admin'])
+	die('Admin privileges required.');
+
+global $boardurl, $boarddir, $modSettings, $sourcedir, $smcFunc, $context;
+
+// Hooks
+$hooks = array(
+	'integrate_pre_include' => '$sourcedir/TopicRenamer.php',
+	'integrate_actions' => 'TopicRenamer_actions',
+	'integrate_mod_buttons' => 'TopicRenamer_mod_buttons',
+	'integrate_load_permissions' => 'TopicRenamer_permissions',
+);
+
+if (!empty($context['uninstalling']))
+	$call = 'remove_integration_function';
+else
+	$call = 'add_integration_function';
+
+foreach ($hooks as $hook => $function)
+	$call($hook, $function);
+
+if (SMF == 'SSI')
+	echo 'Database changes are complete! Please wait...';
+
+?>
